62693ffc11a936b87f603343d97519b50b8eaedb
Lizeth Ramírez <101681059+LZeth@users.noreply.github.com>
2023-10-05 13:52:32 -0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+import { Button } from './components/button';
+import { useState } from 'react';
+
+function App({ texto }) {
+  const [estado, setEstado] = useState("Verdadero")
+  return (
+    <>
+      <div> 
+        <h1>
+        {estado}
+        </h1>
+        
+        <button 
+         onClick ={()=> {
+          if (estado == "Verdadero"){
+            setEstado("Falso");
+          } else {
+            setEstado("Verdadero");
+          }
+        }}
+        >
+          Cambiar Estado
+        </button>
+
+        <Button texto = {"Compras"}></Button>
+      </div>
+    </>
+  )
+}
+
+export default App;
